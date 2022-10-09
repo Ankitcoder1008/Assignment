0cbe437333ca01303387d19cbf2e80cbe7022b24
Ankitcoder1008 <115339749+Ankitcoder1008@users.noreply.github.com>
2022-10-09 11:32:07 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,15 @@
+# my_list = [10,20,30,40,50]
+# print(my_list[-1])
+
+# Var1 = "Good"
+# Var2 = "Morning"
+# Var3 = Var1 + Var2
+# print(Var3)
+# test = 1
+# print(test,'is', bool(test))
+my_list1 = ['cow','goat','buffalo','parrot']
+my_list2 = [1,2,3,4,5]
+my_list1.extend(my_list2)
+
+
+print(my_list1)
